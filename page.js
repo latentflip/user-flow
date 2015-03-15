7de1c9187b5f9de32e6feee282c6060e3371cfa4
Philip Roberts <phil@latentflip.com>
2015-03-15 19:22:30 +0000

    Remove bindAll since was a dumb idea

--- page.js
+++ page.js
@@ -8,35 +8,20 @@ class Page {
         }
     }
 
-    bindAll() {
-        let keys = Object.getOwnPropertyNames(this.__proto__);
-        var omit = [
-            'constructor',
-            'initialize'
-        ];
-
-        for (let idx in keys) {
-            let key = keys[idx];
-            if (typeof this[key] === 'function' && omit.indexOf(key) === -1) {
-                this[key] = this[key].bind(this);
-            }
-        }
-    }
-
     sequence() {
         return Promise.resolve();
     }
 
     query(selector) {
         return new ElementProxy.Single({
-            client: this.client, 
+            client: this.client,
             selector: selector
         });
     }
 
     queryAll(selector) {
         return new ElementProxy.Multiple({
-            client: this.client, 
+            client: this.client,
             selector: selector
         });
     }
